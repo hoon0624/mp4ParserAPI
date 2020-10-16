72a6462a51d61592e56f794c653a63c21a5f8e53
simpson9241 <simpson9241@gmail.com>
2020-10-16 11:31:27 +0900

    test 3

---
+++ src/main/java/com/mp4Parser/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.mp4Parser;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController{
+  @RequestMapping("/welcome")
+  public String welcome(){
+    return "welcome"l;
+  }
+}
