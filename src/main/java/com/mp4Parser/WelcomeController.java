ce0a10086b1ac71caa99413bbf24d8650548164c
simpson9241 <simpson9241@gmail.com>
2020-10-16 11:32:17 +0900

    test 3

--- src/main/java/com/mp4Parser/WelcomeController.java
+++ src/main/java/com/mp4Parser/WelcomeController.java
@@ -3,7 +3,7 @@ package com.mp4Parser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@RestController
 public class WelcomeController{
   @RequestMapping("/welcome")
   public String welcome(){
